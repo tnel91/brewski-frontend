a2ff74d4970e6d9f675d4c40a6915f24f835f411
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-11-14 09:52:39 -0500

    Added routing for existing pages and also created Profile page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css'
 import Nav from './components/Navbar'
 import { useState } from 'react'
+import Home from './pages/Home'
 import Signin from './pages/Signin'
 import Register from './pages/Register'
+import Breweries from './pages/Breweries'
+import Profile from './pages/Profile'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [authenticated, toggleAuthenticated] = useState(false)
@@ -22,6 +26,14 @@ function App() {
         handleLogout={handleLogout}
       />
       <h1>Brewski</h1>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/breweries" element={<Breweries />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   )
 }
