da00d65e4ddb80d7085b516119cd32922d01957d
Travis Nelson <tjnel6691@gmail.com>
2022-11-14 16:52:50 -0500

    Review form built. Needs handle submit function

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,9 +1,35 @@
+import { useState } from 'react'
+
 const ReviewForm = () => {
+  const initialState = {
+    body: ''
+  }
+
+  const [formState, setFormState] = useState(initialState)
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    //Submit logic here
+    console.log('submitted')
+    //
+    setFormState(initialState)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <fieldset>
         <legend>Create Review</legend>
-        <textarea id="review_body" placeholder="Review" required></textarea>
+        <textarea
+          id="body"
+          placeholder="Review"
+          onChange={handleChange}
+          value={formState.body}
+          required
+        ></textarea>
         <button type="submit">Submit Review</button>
       </fieldset>
     </form>
