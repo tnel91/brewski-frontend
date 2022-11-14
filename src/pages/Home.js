7b5062cdcf148d63c10b47d375aeb5ae033efeba
james-fleming394 <james.fleming394@gmail.com>
2022-11-14 12:32:15 -0500

    Home page styling updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,18 +6,18 @@ let navigate = useNavigate();
 
   return (
     <div className="home">
-      <img src ="https://i.imgur.com/BlhTJn5.png" alt="brewski logo" />
-      <div>
-        <p>Welcome to Brewski!</p>
-        <p>Brewski is an online application that allows you to review Breweries around the world!  This application shares your review with thousands of members in real time and allows you to save reviews of Breweries you find interesting!   </p>
+      <img className="home-picture" src ="https://i.imgur.com/doHIjDt.png" alt="brewski logo" />
+      <div className="writing">
+        <h2>Welcome to Brewski!</h2>
+        <p>Brewski is an online application that allows you to review Breweries around the world! <br></br> This application shares your review with thousands of members in real time and allows you to save reviews of Breweries you find interesting!   </p>
       </div>
-      <div className="home-button">
-        <button onClick={() => navigate('/signin')}>Click Here To Sign In</button>
-        <button onClick={() => navigate('/register')}>Click Here To Register</button>
+      <div className="home-buttons">
+        <button id="button" onClick={() => navigate('/signin')}>Click Here To Sign In</button>
+        <button id="button" onClick={() => navigate('/register')}>Click Here To Register</button>
       </div>
       <div className="home2">
-      <img src ="https://dmn-dallas-news-prod.cdn.arcpublishing.com/resizer/CwyNjYZJWmvmEs9vgVHcov1XpfU=/1660x934/filters:focal(2007x3531:2017x3521):no_upscale()/cloudfront-us-east-1.images.arcpublishing.com/dmn/JBCC4KTD4FDCHFLACMTQTW4GBE.jpg" alt="beer review" />
-      <img src="https://s3.amazonaws.com/images.gearjunkie.com/uploads/2021/11/DN5A4037-2400x1600.jpg" alt="beer review" />
+      <img className="pic1" src ="https://dmn-dallas-news-prod.cdn.arcpublishing.com/resizer/CwyNjYZJWmvmEs9vgVHcov1XpfU=/1660x934/filters:focal(2007x3531:2017x3521):no_upscale()/cloudfront-us-east-1.images.arcpublishing.com/dmn/JBCC4KTD4FDCHFLACMTQTW4GBE.jpg" alt="beer review" />
+      <img className="pic2" src="https://s3.amazonaws.com/images.gearjunkie.com/uploads/2021/11/DN5A4037-2400x1600.jpg" alt="beer review" />
       </div>
     </div>
   )
