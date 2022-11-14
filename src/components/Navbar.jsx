2495ca64c3988bcc24a4cef75b42e71e6d74a2da
james-fleming394 <james.fleming394@gmail.com>
2022-11-14 15:47:41 -0500

    User profile page structure outlined

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Nav = ({ authenticated, user, handleLogout}) => {
     if (user) {
         authenticatedOptions = (
             <nav>
-                <h2>Welcome {user.id}</h2>
+                <h2>Welcome!</h2>
                 <Link className="link" to="/breweries">Breweries</Link>
                 <Link className="link" to="/profile">Profile</Link>
                 <Link className="link" onClick={handleLogout} to="/">Sign Out</Link>
