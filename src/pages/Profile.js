0219af11b5f5052d03d379dd9ba5467485dc43f8
james-fleming394 <james.fleming394@gmail.com>
2022-11-15 09:16:58 -0500

    CSS to user profile worked on

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  
+  let navigate = useNavigate();
+
   return (
     <div className="profile">
-      <div className="user-info">
+      <div className="info-card">
       <h2>User Profile</h2>
-      <div id="user1">
+      <div id="user">
         <h4>Email:</h4>
         <h4>Reviews Written:</h4>
         <h4>Last Seen:</h4>
       </div>
       </div>
-      <div className="user2">
+      <div className="review-cardfav">
         <h2>Favorite Reviews</h2>
         <div id="fav-reviews">
           <h4>Example Review Title: Fifth Hammer Brewing Company</h4>
@@ -20,7 +22,7 @@ const Profile = () => {
             I highly recommend the Functional Modern for an IPA lover and the Break of Jawn if you're into Belgian Wheat styled beers.  Can't wait to go back! </p>
         </div>
       </div>
-      <div className="user23">
+      <div className="review-card">
         <h2>Your Reviews</h2>
         <div id="your-reviews">
         <h4>Example Review Title: Fifth Hammer Brewing Company</h4>
@@ -30,6 +32,9 @@ const Profile = () => {
             <h4>Example Review Title: Captain Lawrence Brewing Company</h4>
             <p>CL Brewing Co was a great experience!  Can't wait to head back for my CL beers and treats!</p>
         </div>
+      </div>
+      <div className="user-button">
+      <button id="user-button" onClick={() => navigate('/breweries')}>Write a New Review</button>
       </div>
     </div>
   )
