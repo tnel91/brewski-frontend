d35a2b3d1a8764323e17199b71a003f3490608a1
Travis Nelson <tjnel6691@gmail.com>
2022-11-17 11:22:20 -0500

    Update review functionality added

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,12 +1,22 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { BASE_URL } from '../globals'
+import { useNavigate } from 'react-router-dom'
 
 const ReviewForm = (props) => {
+  let navigate = useNavigate()
+
   const [formState, setFormState] = useState({
     body: '',
     breweryId: props.breweryId,
     authorId: ''
+  })
+
+  const [formLayout, setFormLayout] = useState({
+    legend: 'Create Review',
+    buttonText: 'Submit Review',
+    updateForm: false,
+    reveiwId: null
   })
 
   const handleChange = (event) => {
@@ -15,25 +25,64 @@ const ReviewForm = (props) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault()
-    await axios
-      .post(`${BASE_URL}/reviews/new`, formState)
-      .then(() => {
-        props.getReviews()
+    if (formLayout.updateForm === true) {
+      await axios
+        .put(`${BASE_URL}/reviews/edit/${formLayout.reveiwId}`, formState)
+        .then(() => {
+          props.getReviews()
+        })
+        .catch((error) => {
+          alert(error)
+        })
+      setFormState({ ...formState, body: '' })
+      navigate('/profile')
+    } else {
+      await axios
+        .post(`${BASE_URL}/reviews/new`, formState)
+        .then(() => {
+          props.getReviews()
+        })
+        .catch((error) => {
+          alert(error)
+        })
+      setFormState({
+        body: '',
+        breweryId: props.breweryId,
+        authorId: ''
       })
-      .catch((error) => {
-        alert(error)
-      })
-    setFormState({
-      body: '',
-      breweryId: props.breweryId,
-      authorId: ''
-    })
+      navigate('/profile')
+    }
   }
 
-  const initForm = () => {
+  const initForm = async () => {
     if (props.user) {
-      setFormState({ ...formState, authorId: props.user.id })
       document.getElementById('review_form').style.display = ''
+      await axios
+        .get(`${BASE_URL}/reviews/${props.breweryId}/${props.user.id}`)
+        .then((response) => {
+          if (response.data) {
+            setFormState({
+              body: response.data.body,
+              breweryId: props.breweryId,
+              authorId: props.user.id
+            })
+            setFormLayout({
+              legend: 'Update Review',
+              buttonText: 'Update Review',
+              updateForm: true,
+              reveiwId: response.data.id
+            })
+          } else {
+            setFormState({
+              body: '',
+              breweryId: props.breweryId,
+              authorId: props.user.id
+            })
+          }
+        })
+        .catch((error) => {
+          alert(error)
+        })
     } else {
       document.getElementById('review_form').style.display = 'none'
     }
@@ -41,12 +90,12 @@ const ReviewForm = (props) => {
 
   useEffect(() => {
     initForm()
-  }, [props.user])
+  }, [props.user, props.breweryId])
 
   return (
     <form id="review_form" onSubmit={handleSubmit}>
       <fieldset>
-        <legend>Create Review</legend>
+        <legend>{formLayout.legend}</legend>
         <textarea
           id="body"
           placeholder="Review"
@@ -54,7 +103,7 @@ const ReviewForm = (props) => {
           value={formState.body}
           required
         ></textarea>
-        <button type="submit">Submit Review</button>
+        <button type="submit">{formLayout.buttonText}</button>
       </fieldset>
     </form>
   )
